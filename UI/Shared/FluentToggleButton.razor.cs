b27018ac9b91a86eee5a1ea4e8cc51a1c8b6548d
Erica Abenti <emmert.math@gmail.com>
2024-02-18 22:05:50 -0600

    working nav toggle

--- UI/Shared/FluentToggleButton.razor.cs
+++ UI/Shared/FluentToggleButton.razor.cs
@@ -1,28 +1,28 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.FluentUI.AspNetCore.Components;
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.FluentUI.AspNetCore.Components;
 
-namespace UI.Shared;
-public partial class FluentToggleButton
-{
-	[EditorRequired, Parameter]
-	public string ToggleOnTitle { get; set; } = string.Empty;
+//namespace UI.Shared;
+//public partial class FluentToggleButton
+//{
+//	[EditorRequired, Parameter]
+//	public string ToggleOnTitle { get; set; } = string.Empty;
 
-	[EditorRequired, Parameter]
-	public string ToggleOffTitle { get; set; } = string.Empty;
+//	[EditorRequired, Parameter]
+//	public string ToggleOffTitle { get; set; } = string.Empty;
 
-	[EditorRequired, Parameter]
-	public Icon? ToggleOnIcon { get; set; }
+//	[EditorRequired, Parameter]
+//	public Icon? ToggleOnIcon { get; set; }
 
-	[EditorRequired, Parameter]
-	public Icon? ToggleOffIcon { get; set; }
+//	[EditorRequired, Parameter]
+//	public Icon? ToggleOffIcon { get; set; }
 
-	[EditorRequired, Parameter]
-	public virtual bool IsToggledOn { get; set; } = false;
+//	[EditorRequired, Parameter]
+//	public virtual bool IsToggledOn { get; set; } = false;
 
-	[Parameter]
-	public virtual EventCallback<bool> IsToggledOnChanged { get; set; }
+//	[Parameter]
+//	public virtual EventCallback<bool> IsToggledOnChanged { get; set; }
 
-	string ToggledOnClass => UI.Constants.Classes.Display(IsToggledOn);
+//	string ToggledOnClass => UI.Constants.Classes.Display(IsToggledOn);
 
-	string ToggledOffClass => UI.Constants.Classes.DisplayNone(IsToggledOn);
-}
+//	string ToggledOffClass => UI.Constants.Classes.DisplayNone(IsToggledOn);
+//}
